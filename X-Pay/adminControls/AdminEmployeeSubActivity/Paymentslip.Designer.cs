f4940d7c1fd3c6a8114918c7b98daafe1cae1f11
MPrajakaruna <140785174+MPrajakaruna@users.noreply.github.com>
2024-05-02 23:24:43 +0530

    new

---
+++ X-Pay/adminControls/AdminEmployeeSubActivity/Paymentslip.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace X_Pay.AdminControls.AdminEmployeeSubActivity
+{
+    partial class Paymentslip
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Paymentslip));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.closeico = new System.Windows.Forms.PictureBox();
+            this.minico = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.titleico = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.closeico)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minico)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.titleico)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.closeico);
+            this.panel1.Controls.Add(this.minico);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.titleico);
+            this.panel1.Location = new System.Drawing.Point(-1, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(402, 33);
+            this.panel1.TabIndex = 2;
+            // 
+            // closeico
+            // 
+            this.closeico.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeico.Image = ((System.Drawing.Image)(resources.GetObject("closeico.Image")));
+            this.closeico.Location = new System.Drawing.Point(363, 5);
+            this.closeico.Name = "closeico";
+            this.closeico.Size = new System.Drawing.Size(25, 25);
+            this.closeico.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.closeico.TabIndex = 14;
+            this.closeico.TabStop = false;
+            // 
+            // minico
+            // 
+            this.minico.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.minico.Image = ((System.Drawing.Image)(resources.GetObject("minico.Image")));
+            this.minico.Location = new System.Drawing.Point(332, 5);
+            this.minico.Name = "minico";
+            this.minico.Size = new System.Drawing.Size(25, 25);
+            this.minico.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.minico.TabIndex = 13;
+            this.minico.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(52)))), ((int)(((byte)(88)))));
+            this.label1.Location = new System.Drawing.Point(40, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "X Pay - Payment ";
+            // 
+            // titleico
+            // 
+            this.titleico.Image = ((System.Drawing.Image)(resources.GetObject("titleico.Image")));
+            this.titleico.Location = new System.Drawing.Point(17, 9);
+            this.titleico.Name = "titleico";
+            this.titleico.Size = new System.Drawing.Size(15, 15);
+            this.titleico.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.titleico.TabIndex = 10;
+            this.titleico.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(18)))), ((int)(((byte)(60)))));
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(12, 46);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(375, 148);
+            this.panel2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(280, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Submit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Upload";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(14, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(294, 18);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "payment of completed task.Thank You.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(14, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(295, 18);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Please add a payment slip for the each";
+            // 
+            // Paymentslip
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Paymentslip";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Paymentslip";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.closeico)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minico)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.titleico)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox closeico;
+        private System.Windows.Forms.PictureBox minico;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox titleico;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
